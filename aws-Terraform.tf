52ac3ce8ace996808126b845caf2f68489da50e4
ritanshu jayant <67803038+ritanshuj@users.noreply.github.com>
2021-09-14 15:04:08 +0530

    Update aws-Terraform.tf

--- aws-Terraform.tf
+++ aws-Terraform.tf
@@ -1,1 +1,12 @@
 
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA6ICFJEQHMZDD3HEU"
+  secret_key = "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"
+
+}
+
+resource "aws_instance" "name2" {
+   ami               = "ami-0c1a7f89451184c8b"
+   instance_type     = "t2.micro"
+   }
