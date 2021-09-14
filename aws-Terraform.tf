47227adeb9f765c88a43ce16245523ab16fd547c
ritanshu jayant <67803038+ritanshuj@users.noreply.github.com>
2021-09-14 18:09:44 +0530

    Update aws-Terraform.tf

--- aws-Terraform.tf
+++ aws-Terraform.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6ICFJEQHMZDD3HEU"
-  secret_key = "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"
+  access_key = "AKIA6ICFJEQHJPITPUMF"
+  secret_key = "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"
 
 }
 
